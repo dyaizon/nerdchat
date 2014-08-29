cd5bad33522957675b1408f75f74f20ea83b0b03
Joseph Becher <jwbecher@gmail.com>
2014-08-29 12:47:12 -0400

    Cleaner getPlayer to prepare for linked worlds support

--- src/main/java/net/jwebnet/nerdchat/NerdChat.java
+++ src/main/java/net/jwebnet/nerdchat/NerdChat.java
@@ -16,9 +16,6 @@
  */
 package net.jwebnet.nerdchat;
 
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -27,6 +24,8 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
 
 public class NerdChat
         extends JavaPlugin
@@ -59,41 +58,35 @@ public class NerdChat
         // Clear the list of players who will see the message
         evt.getRecipients().clear();
 
-        // Create a new list to hold message recipients
-        List<Player> recipients = new LinkedList<Player>();
+        /*
+         * Get a list of all players in the sender's world
+         */
+        List<Player> senderWorldPlayers;
+        senderWorldPlayers = senderWorld.getPlayers();
 
-//        for (World world : Bukkit.getWorlds()) {
-        // Create a list of players for this world
-        Player[] players;
-        // Fill the list with the players from this world
-        // Testing https://forums.bukkit.org/threads/java-util-concurrentmodificationexception-error.92020/
-        players = Bukkit.getOnlinePlayers();
-        // Loop though the players
-        for (Player p : players) {
-            // Get the World of Player p
-            World playerWorld;
-            playerWorld = p.getWorld();
-            try {
-                if (playerWorld == senderWorld) {
-                    // If player is in the sender's world, add them
-                    recipients.add(p);
-                } else if ((senderPlayer.getWorld().getName().equals("Mineworld".toLowerCase()))
-                        || (p.getWorld().getName().equals("vip".toLowerCase()))) {
-                    /* If sender is in either "Mineworld" or "vip"
-                     * this check if the player is also in either world, and add.
-                     */
-                    if ((playerWorld.getName().equals("Mineworld".toLowerCase()))
-                            || (playerWorld.getName().equals("vip".toLowerCase()))) {
-                        recipients.add(p);
-                    }
-                }
-            } catch (ConcurrentModificationException localConcurrentModificationException) {
+        /*
+         * Add the players in the sender's world to the list
+         */
+        evt.getRecipients().addAll(senderWorldPlayers);
+
+        /*
+         * Check if sender is in a linked world
+         */
+        if ((senderWorld.getName().equals("Mineworld".toLowerCase()))
+                || (senderWorld.getName().equals("vip".toLowerCase()))) {
+            /*
+             * sender is in a linked world
+             */
+            if (senderWorld.getName().equals("Mineworld")) {
+                /* If sender is in either "Mineworld" or "vip"
+                 * this check if the player is also in either world, and add.
+                 */
+                evt.getRecipients().addAll(Bukkit.getWorld("Mineworld").getPlayers());
             }
 
-        }
-//        }
-        // Add the list of recipients back to the event
-        evt.getRecipients().addAll(recipients);
+        } else if (senderWorld.getName().equals("vip")) {
+            evt.getRecipients().addAll(Bukkit.getWorld("Mineworld").getPlayers());
 
+        }
     }
 }
